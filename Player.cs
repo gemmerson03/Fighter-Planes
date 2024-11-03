836c5dd03f72eee981da392710fe91313a64bcbc
Grace Emmerson <gemmerson03@gmail.com>
2024-11-03 15:35:07 -0500

    updated y movement to stop at the bottom and the top of the screen

--- Player.cs
+++ Player.cs
@@ -43,11 +43,16 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
 
-        if (transform.position.y > 8f || transform.position.y <= -8f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
-        }
+           if (transform.position.y >= 0f)
+    {
+        transform.position = new Vector3(transform.position.x, 0, 0);
     }
+
+    if (transform.position.y <= -4f)
+    {
+        transform.position = new Vector3(transform.position.x, -4, 0);
+    }
+}
 
     void Shooting()
     {
